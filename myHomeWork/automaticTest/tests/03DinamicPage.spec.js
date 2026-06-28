5128b34c2109494af59847d3d84b54a2a66a2837
Rodolfo <rodolfowb32@gmail.com>
2026-06-28 17:58:23 +0300

    codifico test 03 tabla dinamica con exito

--- myHomeWork/automaticTest/tests/03DinamicPage.spec.js
+++ myHomeWork/automaticTest/tests/03DinamicPage.spec.js
@@ -9,18 +9,35 @@ test.only('Dinamic pagination table', async ({ page }) => {
     await page.locator('.sorting_asc').waitFor();
 
     await page.locator('[name="example_length"]').waitFor();
-    /*await page.locator('[name="example_length"]').selectOption(optionSelect[0]);
-    
-    console.log(await page.locator(".sorting_1").allTextContents());*/
-
-    console.log(await page.locator("[name='example_length']").length());
-
+    //await page.locator('[name="example_length"]').selectOption(optionSelect[0]);    
     for(i = 0; i < optionSelect.length; i++){
         await page.locator('[name="example_length"]').selectOption(optionSelect[i]);
         
-        if(optionSelect[i] === "5"){
+        if(optionSelect[i] === "3"){
+            console.log(await page.locator(".sorting_1").allTextContents()); 
+            console.log(await page.locator(".odd td").nth(1).textContent());
+            console.log(await page.locator(".even td").nth(1).textContent()); 
+            console.log(await page.locator(".odd td").nth(7).textContent());          
+        } else if(optionSelect[i] === "5"){
             console.log(await page.locator(".sorting_1").allTextContents());
-            break;
-        }            
+            console.log(await page.locator(".odd td").nth(3).textContent());
+            console.log(await page.locator(".even td").nth(3).textContent()); 
+            console.log(await page.locator(".odd td").nth(9).textContent());            
+            console.log(await page.locator(".even td").nth(9).textContent()); 
+            console.log(await page.locator(".odd td").nth(15).textContent());            
+        } else if(optionSelect[i] === "-1"){
+            console.log(await page.locator(".sorting_1").allTextContents());
+            console.log(await page.locator(".odd td").nth(5).textContent());
+            console.log(await page.locator(".even td").nth(5).textContent()); 
+            console.log(await page.locator(".odd td").nth(11).textContent());            
+            console.log(await page.locator(".even td").nth(11).textContent()); 
+            console.log(await page.locator(".odd td").nth(17).textContent());   
+            
+            console.log(await page.locator(".even td").nth(17).textContent()); 
+            console.log(await page.locator(".odd td").nth(23).textContent());            
+            console.log(await page.locator(".even td").nth(23).textContent()); 
+            console.log(await page.locator(".odd td").nth(29).textContent());
+            console.log(await page.locator(".even td").nth(29).textContent());          
+        }
     }
 });
